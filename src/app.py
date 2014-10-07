12a6c979c7648b9fa43165286afebac9e8df7101
ambidextrousTx <ravisinha@my.unt.edu>
2014-10-06 23:09:04 -0400

    Add method stubs for sending and receiving tasks to/ from the database

--- src/app.py
+++ src/app.py
@@ -11,5 +11,15 @@ app = Flask(__name__)
 def index():
     return 'A work in progress, check back later'
 
+@app.route('/add', methods=['POST'])
+def add_reminder():
+    return 'Use this method to POST new reminders to the database'
+
+@app.route('/show', methods=['GET'])
+def show_reminders():
+    return 'Use this method to GET all the reminders in the database' \
+            'in a sticky format'
+
+
 if __name__ == '__main__':
     app.run()
